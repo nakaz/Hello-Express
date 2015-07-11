ac2c214429b03e43347912bbc77d84e16ff2c007
Sean Nakamura <seanenakamura@gmail.com>
2015-07-11 10:04:29 -1000

    write to DOM

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
+app.set('view engine', 'jade');
+app.set('views');
 app.get('/', function (req, res){
-  res.send('Hello World!');
+  res.render('index', {title: 'Hello-Express'});
 });
 
 var server = app.listen(3000, function(){
