58bb143be7bf48e9e0fc49b43afb790e6cd1d65c
Sean Nakamura <seanenakamura@gmail.com>
2015-07-11 20:03:36 -1000

    passport test working

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+
 var config = require('./config/config.json');
 
 app.use(express.static('public'));
@@ -26,6 +30,29 @@ app.use(session({
   }
 }));
 
+app.use(passport.initialize());
+// app.use(passport.session());
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    console.log(username, password);
+    var admin = config.admin;
+
+    if(!admin){
+      return done(new Error('No admin configured!'));
+    }
+
+    if (username !== admin.username) {
+      return done(null, false, { message: 'Incorrect username.' });
+    }
+    if (password !== admin.password) {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+
+    return done(null, admin);
+  }
+));
+
 app.use(function (req, res, next){
   var session = req.session;
   next();
@@ -38,6 +65,16 @@ app.get('/', function (req, res){
 app.post('/', function (req, res){
   req.session.username = req.body.username;
   res.redirect('/');
+});
+
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/secret',
+                                   failureRedirect: '/',
+                                   session: false})
+);
+
+app.get('/secret', function(req, res, next){
+  res.send('SECRET!');
 });
 
 var server = app.listen(3000, function(){
