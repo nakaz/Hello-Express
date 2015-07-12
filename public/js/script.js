25ecf9e90da5c64b843803b1fb5c068fb43ab606
Sean Nakamura <seanenakamura@gmail.com>
2015-07-11 17:40:01 -1000

    changed to japanese for no reason

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $('#button').click(function(){
-    alert('IT WORKS!');
+    alert('やった〜！');
   });
 });
--- server.js
+++ server.js
@@ -36,8 +36,7 @@ app.get('/', function (req, res){
 });
 
 app.post('/', function (req, res){
-  var username = req.body.username;
-  req.session.username = username;
+  req.session.username = req.body.username;
   res.redirect('/');
 });
 
